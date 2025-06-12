102171e5add35568540fc1f7689e70bddd1c6a17
MatheusDSantossi <matheus1819.santos20@gmail.com>
2025-06-11 21:07:06 -0300

    feat(navbar): scaffold navabar component using Tailwind CSS and Framer Motion

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './components/Navbar';
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <Navbar />
-    </>
-  )
+    <div className="w-full">
+      <div className="absolute top-0 left-0 w-full">
+        <Navbar />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,40 @@
+import "../App.css";
+import lightLogo from "/logo.svg";
+import darkLogo from "/logo_darker.svg";
+import { SlideTabsExample } from "./SlideTabs";
+
+function Navbar() {
+    return (
+        <nav className="bg-primary">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <img src={lightLogo} className="h-8" alt="Light Hecate Logo" />
+                
+                {/* Access links */}
+                <SlideTabsExample />
+
+                {/* <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+                        <li>
+                            <a href="#" className="block py-2 px-3 text-white rounded-sm md:p-0">About</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block py-2 px-3 text-white rounded-sm md:p-0">Features</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block py-2 px-3 text-white rounded-sm md:p-0">Products</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block py-2 px-3 text-white rounded-sm md:p-0">Register</a>
+                        </li>
+                    </ul>
+                </div> */}
+
+                {/* Get in action button */}
+                <button className="bg-secondary text-primary">See Hecate in Action</button>
+
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar
