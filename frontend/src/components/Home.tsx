ad15f3cbc301ca1689290e9f7f0e957040974875
MatheusDSantossi <matheus1819.santos20@gmail.com>
2025-06-27 11:48:18 -0300

    feat(animations): add animations to Home, WhyHecate & Slidetabs

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,11 +1,22 @@
 import robot from "/home/robot_image.png";
 import computer from "/home/MacBook Air (15 inch).png";
 
+import { motion } from "framer-motion";
+import { tweenIn } from "../lib/animations";
+
 const Home = () => {
   return (
     <div className="relative flex items-center justify-center bg-primary shadow-2xl">
       {/* Left-side robot image */}
-      <img className="hidden lg:block h-[500px] object-contain -ml-25" src={robot} alt="A robot" />
+      <motion.img 
+      className="hidden lg:block h-[500px] object-contain -ml-25" 
+      src={robot} 
+      alt="A robot" 
+      initial={{ x: "-100vw", opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      exit={{ x: 0, opacity: 1 }}
+      transition={{ ...tweenIn, duration: 0.8, ease: [0.42,0,0.58,1], delay: 0.4}}
+      />
 
       {/* Hero text */}
       <div className="flex z-1 py-8 
@@ -17,10 +28,14 @@ const Home = () => {
       </div>
 
       {/* Right-side laptop image */}
-      <img
+      <motion.img
         className="hidden lg:block absolute right-0 h-[700px] object-contain mt-10 pointer-events-none"
         src={computer}
         alt="A macbook showing the initial screen of the Chatbot Hecate"
+        initial={{ x: "100vw", opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        exit={{ x: 0, opacity: 1 }}
+        transition={{ ...tweenIn, duration: 0.8, ease: [0.42,0,0.58,1], delay: 0.4 }}
       />
     </div>
   );
---
+++ frontend/src/utils/WhyHecateIconsData.ts
@@ -0,0 +1,60 @@
+import aiIcon from "/whyHecate/AI-icon.svg";
+import happyIcon from "/whyHecate/happy-icon.svg";
+import infoIcon from "/whyHecate/Info.svg";
+import support24h from "/whyHecate/Support 24h.svg";
+import arrow from "/whyHecate/Arrow.svg";
+
+export interface IconData {
+    src: string;
+    alt: string;
+    className: string;
+}
+
+export const iconData: IconData[] = [
+  /* 1. Central AI bulb at the top center */
+  {
+    src: aiIcon,
+    alt: "AI bulb icon",
+    className: "absolute left-1/2 top-0 w-12 h-12 transform -translate-x-1/2",
+  },
+  /* 2. Left icon (Support 24h) */
+  {
+    src: support24h,
+    alt: "Support 24h icon",
+    className:
+      "absolute left-50 md:left-55 lg:left-65 top-23 w-6 h-6 transform -translate-x-1/2",
+  },
+  /* 3. Right icon (Info) */
+  {
+    src: infoIcon,
+    alt: "Info icon",
+    className:
+      "absolute right-45 md:right-50 lg:right-60 top-23 w-6 h-6 transform -translate-x-1/2",
+  },
+  /* 4. Bottom icon (Happy face) */
+  {
+    src: happyIcon,
+    alt: "Happy face icon",
+    className: "absolute left-1/2 -bottom-3 w-6 h-6 transform -translate-x-1/2",
+  },
+  /* 5. Arrows */
+  /* Arrow pointing from bulb down to happy face */
+  {
+    src: arrow,
+    alt: "Arrow poiting down",
+    className:
+      "absolute left-1/2 top-[4.2rem] w-15 h-10 transform -translate-x-1/2 rotate-90",
+  },
+  /* Arrow from bulb to support (left) */
+  {
+    src: arrow,
+    alt: "Arrow poiting left",
+    className: "absolute left-[33%] top-[3rem] w-15 h-10 transform rotate-150",
+  },
+  /* Arrow from bulb to info (right) */
+  {
+    src: arrow,
+    alt: "Arrow poiting right",
+    className: "absolute right-[33%] top-[3rem] w-15 h-10 transform rotate-30",
+  },
+];
