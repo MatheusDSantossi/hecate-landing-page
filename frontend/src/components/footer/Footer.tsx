b1010c7337a265e62d2c150afc3ae54a59358fdc
MatheusDSantossi <matheus1819.santos20@gmail.com>
2025-07-17 17:58:45 -0300

    feat(footer): complete Footer section layout with responsivity

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -2,12 +2,14 @@ import ShinyLine from "../ourFeatures/ShinyLine";
 import logo_darker from "/logo_darker.svg";
 import copyright from "/footer/Copyright.svg";
 import { Options } from "../../utils/FooterOptions";
+import { motion } from "framer-motion";
+import { fadeInUp } from "../../lib/animations";
 
 const Footer = () => {
   return (
-    <div>
+    <footer>
       <ShinyLine />
-      <div className="bg-secondary pb-8">
+      <div className="bg-secondary pb-8 text-primary">
         <div className="flex align-bottom items-end p-12 mt-10">
           <img
             className="h-20"
@@ -20,21 +22,33 @@ const Footer = () => {
               src={copyright}
               alt="Copyright Icon"
             />
-            <h2 className=" font-clash font-medium text-primary">2025</h2>
+            <h2 className="pointer-events-none font-clash font-medium">
+              2025
+            </h2>
           </div>
         </div>
-        <div className="">
+        <motion.div 
+        className="flex xs:flex-col sm:flex-row xs:mx-30 sm:mx-0 xs:justify-center sm:justify-around md:justify-center"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.4 }}
+        variants={fadeInUp}
+        >
           {Options.map(({ title, option1, option2, option3 }) => (
-            <div className="flex text-primary">
-              <h2 className="font-medium">{title}</h2>
-              <h3>{option1}</h3>
-              <h3>{option2}</h3>
-              <h3>{option3}</h3>
+            <div className="flex flex-col mb-5 md:mr-16">
+              <h2 className="font-medium mb-3 pointer-events-none">{title}</h2>
+              <h3 className="cursor-pointer hover:underline">{option1}</h3>
+              <h3 className="cursor-pointer hover:underline">{option2}</h3>
+              <h3 className="cursor-pointer hover:underline">{option3}</h3>
             </div>
           ))}
+        </motion.div>
+        <div className="flex items-center mt-10 text-sm">
+          <img className="h-3 ml-4 mr-1" src={copyright} alt="Copyright Icon" />
+          <h3 className="font-medium">matheusd.santosr 2025</h3>
         </div>
       </div>
-    </div>
+    </footer>
   );
 };
 
